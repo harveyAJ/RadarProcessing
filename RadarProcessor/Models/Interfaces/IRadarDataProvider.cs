3742e5f6fd7993db4214671ef21ca7e66860aa73
valentin <valentin.roy@mclaren.com>
2021-03-24 09:15:11 +0000

    add iradardataprovider

---
+++ RadarProcessor/Models/Interfaces/IRadarDataProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace RadarProcessor.Models.Interfaces
+{
+    public interface IRadarDataProvider<T>
+    {
+        /// <summary>
+        ///     Fetches the total number of items available.
+        /// </summary>
+        /// <returns></returns>
+        int FetchCount();
+
+        /// <summary>
+        ///     Fetches a range of items.
+        /// </summary>
+        /// <param name="startIndex">The start index.</param>
+        /// <param name="count">The number of items to fetch.</param>
+        /// <returns></returns>
+        IList<T> FetchRange(int startIndex, int count);
+    }
+}
